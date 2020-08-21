0a4d96ab565476dae88887a1c8d82a491c637227
deonis <denus.reshetnuk@gmail.com>
2020-08-21 21:32:56 +0300

    db details

--- wp-config.php
+++ wp-config.php
@@ -22,16 +22,17 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+//test
+define( 'DB_NAME', 'heroku_82f972d38a897ff' );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', 'b569d3800a50a3' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', '999b6de3' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-east-02.cleardb.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
